0b7559fb915556a6c8a12c44144951d00ad2314a
mehdi361 <mhd.mosavi@gmail.com>
2016-03-05 15:50:38 +0330

    add new model

--- o2_api/models.py
+++ o2_api/models.py
@@ -11,8 +11,16 @@ class Tournament(models.Model):
     class Meta:
         ordering = ('end_date',)
 
+class GameUser(models.Model):
+    user = models.OneToOneField('auth.User', related_name='games',null=True)
+    uuid = models.CharField(max_length=100, unique=True)
+    phone_number = models.CharField(max_length=13)
+    user_verified = models.BooleanField()
+    created_at = models.DateTimeField(auto_now=True)
+    gem_quantity = models.IntegerField()
+
 class Game(models.Model):
-    owner = models.ForeignKey('auth.User', related_name='games')
+    owner = models.ForeignKey(GameUser)
     tournament = models.ForeignKey(Tournament)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
@@ -20,3 +28,19 @@ class Game(models.Model):
 
     class Meta:
         ordering = ('score', 'owner')
+
+class UserVerified(models.Model):
+    user = models.ForeignKey(GameUser)
+    verified_code = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now=True)
+
+class SendMessage(models.Model):
+    user = models.ForeignKey(GameUser)
+    message = models.CharField(max_length=500)
+    message_status = models.CharField(max_length=5)
+
+class BuyPackage(models.Model):
+    user = models.ForeignKey(GameUser)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    gem_quantity = models.IntegerField()
